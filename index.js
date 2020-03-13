8c85800932c07868784c62dd13fa59ee8570bf7d
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2020-03-13 22:35:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,65 @@
+// Imports
 const express = require('express');
 const hbs = require('hbs'); 
+
+
+// Create application
 const app = express();
 const port = 3000;
-
 app.set('view engine', 'hbs');
-
 app.use(express.static('views'));
-
 hbs.registerPartials(__dirname + '/views/partials');
 
+
+// Get login page
 app.get('/', function(req, res) {
     res.render('login', {});
 });
 
+
+// Rider pages
+app.get('/home', function(req, res) {
+    res.render('home', {});
+});
+
+app.get('/reserve', function(req, res) {
+    res.render('login', {});
+});
+
+app.get('/reserve-success', function(req, res) {
+    res.render('login', {});
+});
+
+app.get('/profile', function(req, res) {
+    // var username = req.params.title;
+    res.render('profile', {});
+});
+
+
+// Admin pages
+app.get('/home-admin', function(req, res) {
+    res.render('home-admin', {});
+});
+
+app.get('/reserve-admin', function(req, res) {
+    res.render('login', {});
+});
+
+app.get('/reserve-success-admin', function(req, res) {
+    res.render('login', {});
+});
+
+app.get('/edit-admin', function(req, res) {
+    res.render('edit-admin', {});
+});
+
+app.get('/profile-admin', function(req, res) {
+    // var username = req.params.title;
+    res.render('profile-admin', {});
+});
+
+
+// Helpers
 hbs.registerHelper('cap', function(text) {
     return text.toUpperCase(); 
 });
@@ -22,6 +69,8 @@ hbs.registerHelper('italicize', function(text) {
     return new hbs.SafeString(x); 
 });
 
+
+// Listener
 app.listen(port, function() {
     console.log('App listening to ' + port);
 });
