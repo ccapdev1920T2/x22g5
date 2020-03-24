62899cadd599d8b5683f99481669420decd0ce21
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2020-03-24 21:54:41 +0800

    Added handlebars

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ app.get('/signup', function(req, res) {
 
 // Rider pages
 app.get('/home', function(req, res) {
-    res.render('home', {});
+    var firstname = 'Jeno';
+
+    res.render('home', {
+        firstname: firstname
+    });
 });
 
 app.get('/reserve', function(req, res) {
@@ -40,13 +44,31 @@ app.get('/reserve-success', function(req, res) {
 
 app.get('/profile', function(req, res) {
     // var username = req.params.username;
-    res.render('profile', {});
+    var firstname = 'Jeno';
+    var lastname = 'Lee';
+    var priority = '1';
+    var username = 'jenolee';
+    var email = 'lee_jeno@dlsu.edu.ph';
+    var icon_rider = '/assets/img/rider.png';
+
+    res.render('profile', {
+        firstname: firstname,
+        lastname: lastname,
+        priority: priority,
+        username: username,
+        email: email,
+        icon: icon_rider
+    });
 });
 
 
 // Admin pages
 app.get('/home-admin', function(req, res) {
-    res.render('home-admin', {});
+    var firstname_admin = 'Willy';
+
+    res.render('home-admin', {
+        firstname: firstname_admin
+    });
 });
 
 app.get('/reserve-admin', function(req, res) {
@@ -63,7 +85,19 @@ app.get('/edit-admin', function(req, res) {
 
 app.get('/profile-admin', function(req, res) {
     // var username = req.params.username;
-    res.render('profile-admin', {});
+    var firstname_admin = 'Willy';
+    var lastname_admin = 'Wonka';
+    var username_admin = 'willywonka';
+    var email_admin = 'willy_wonka@dlsu.edu.ph';
+    var icon_admin = 'assets/img/admin.png';
+
+    res.render('profile-admin', {
+        firstname: firstname_admin,
+        lastname: lastname_admin,
+        username: username_admin,
+        email: email_admin,
+        icon: icon_admin
+    });
 });
 
 
@@ -75,6 +109,12 @@ hbs.registerHelper('cap', function(text) {
 hbs.registerHelper('italicize', function(text) {
     var x = '<i>' + text.toUpperCase() + '</i>';
     return new hbs.SafeString(x); 
+});
+
+
+// Helpers
+hbs.registerHelper('cap', function(text) {
+    return text.toUpperCase(); 
 });
 
 
