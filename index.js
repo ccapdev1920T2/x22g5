7d10e41c1296a8f4a1eb6cba841c9b740611017c
Aileen Sabellon <aileen.sabellon@gmail.com>
2020-03-26 17:40:06 +0800

    Created route folder (placing all routes here)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require('express');
 const hbs = require('hbs'); 
+const router = require("./routes/router")
 
 
 // Create application
@@ -9,98 +10,9 @@ const port = 3000;
 app.set('view engine', 'hbs');
 app.use(express.static('views'));
 app.use(express.static('public'));
+app.use("/", router);
+
 hbs.registerPartials(__dirname + '/views/partials');
-
-
-// Get login and sign-up pages
-app.get('/', function(req, res) {
-    res.render('login', {});
-});
-
-app.get('/login', function(req, res) {
-    res.render('login', {});
-});
-
-app.get('/signup', function(req, res) {
-    res.render('signup', {});
-});
-
-
-// Rider pages
-app.get('/home', function(req, res) {
-    var firstname = 'Jeno';
-
-    res.render('home', {
-        firstname: firstname
-    });
-});
-
-app.get('/reserve', function(req, res) {
-    res.render('reserve', {});
-});
-
-app.get('/reserve-success', function(req, res) {
-    res.render('reserve-success', {});
-});
-
-app.get('/profile', function(req, res) {
-    // var username = req.params.username;
-    var firstname = 'Jeno';
-    var lastname = 'Lee';
-    var priority = 'level 2 Students with Inter-Campus enrolled subjects or thesis';
-    var username = 'jenolee';
-    var email = 'lee_jeno@dlsu.edu.ph';
-    var icon_rider = '/assets/img/rider.png';
-
-    res.render('profile', {
-        firstname: firstname,
-        lastname: lastname,
-        priority: priority,
-        username: username,
-        email: email,
-        icon: icon_rider
-    });
-});
-
-
-// Admin pages
-app.get('/home-admin', function(req, res) {
-    var firstname_admin = 'Willy';
-
-    res.render('home-admin', {
-        firstname: firstname_admin
-    });
-});
-
-app.get('/reserve-admin', function(req, res) {
-    res.render('reserve-admin', {});
-});
-
-app.get('/reserve-success-admin', function(req, res) {
-    res.render('reserve-success-admin', {});
-});
-
-app.get('/edit-admin', function(req, res) {
-    res.render('edit-admin', {});
-});
-
-app.get('/profile-admin', function(req, res) {
-    // var username = req.params.username;
-    var firstname_admin = 'Willy';
-    var lastname_admin = 'Wonka';
-    var username_admin = 'willywonka';
-    var email_admin = 'willy_wonka@dlsu.edu.ph';
-    var icon_admin = 'assets/img/admin.png';
-
-    res.render('profile-admin', {
-        firstname: firstname_admin,
-        lastname: lastname_admin,
-        username: username_admin,
-        email: email_admin,
-        icon: icon_admin
-    });
-});
-
 
 // Helpers
 hbs.registerHelper('cap', function(text) {
---
+++ routes/router.js
@@ -0,0 +1,94 @@
+//Imports
+const express = require("express");
+const router = express.Router();
+
+//Routes
+//Login Pages
+router.get('/', function(req, res) {
+    res.render('login', {});
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', {});
+});
+
+router.get('/signup', function(req, res) {
+    res.render('signup', {});
+});
+
+// Rider pages
+router.get('/home', function(req, res) {
+    var firstname = 'Jeno';
+
+    res.render('home', {
+        firstname: firstname
+    });
+});
+
+router.get('/reserve', function(req, res) {
+    res.render('reserve', {});
+});
+
+router.get('/reserve-success', function(req, res) {
+    res.render('reserve-success', {});
+});
+
+router.get('/profile', function(req, res) {
+    // var username = req.params.username;
+    var firstname = 'Jeno';
+    var lastname = 'Lee';
+    var priority = 'level 2 Students with Inter-Campus enrolled subjects or thesis';
+    var username = 'jenolee';
+    var email = 'lee_jeno@dlsu.edu.ph';
+    var icon_rider = '/assets/img/rider.png';
+
+    res.render('profile', {
+        firstname: firstname,
+        lastname: lastname,
+        priority: priority,
+        username: username,
+        email: email,
+        icon: icon_rider
+    });
+});
+
+
+// Admin pages
+router.get('/home-admin', function(req, res) {
+    var firstname_admin = 'Willy';
+
+    res.render('home-admin', {
+        firstname: firstname_admin
+    });
+});
+
+router.get('/reserve-admin', function(req, res) {
+    res.render('reserve-admin', {});
+});
+
+router.get('/reserve-success-admin', function(req, res) {
+    res.render('reserve-success-admin', {});
+});
+
+router.get('/edit-admin', function(req, res) {
+    res.render('edit-admin', {});
+});
+
+router.get('/profile-admin', function(req, res) {
+    // var username = req.params.username;
+    var firstname_admin = 'Willy';
+    var lastname_admin = 'Wonka';
+    var username_admin = 'willywonka';
+    var email_admin = 'willy_wonka@dlsu.edu.ph';
+    var icon_admin = 'assets/img/admin.png';
+
+    res.render('profile-admin', {
+        firstname: firstname_admin,
+        lastname: lastname_admin,
+        username: username_admin,
+        email: email_admin,
+        icon: icon_admin
+    });
+});
+
+module.exports = router;
