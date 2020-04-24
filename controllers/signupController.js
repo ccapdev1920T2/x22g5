dd80fa2e0e48f9fb1b6a45ea73c5cc8580b44325
Aileen Sabellon <aileen.sabellon@gmail.com>
2020-04-24 13:20:44 +0800

    Validations

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,52 +1,62 @@
 
 const Rider = require('../models/UserModel.js');
-const db = require('../models/db.js');
 
 const signupController = {
 
    
     getSignUp: function (req, res) {
-        res.render('signup');
+        if(req.query.err){
+            res.render('signup',{err: "passwords doesn't match"});
+        }
+        else{
+            res.render('signup');
+        }
     },
 
     postSignUp: function (req, res) {
 
         
-        var firstname = req.body.firstname;
-        var lastname = req.body.lastname;
+        var firstName = req.body.firstName;
+        var lastName = req.body.lastName;
         var username = req.body.username;
         var password = req.body.password;
         var email = req.body.email;
         var confirmPassword = req.body.confirmPassword;
         var priorityLevel = req.body.priorityLevel;
 
-        
-
-        var rider = {
-            firstname: firstname,
-            lastname: lastname,
-            password: password,
-            email: email,
-            username: username,
-            confirmPassword: confirmPassword,
-            priorityLevel: priorityLevel
-        }
-
-
-        db.insertOne(Rider, rider, function(flag) {
-
-               if(flag){
-                console.log("1 document added");
-                res.redirect('/');
-               }
-               else{
-                   console.log("Error in input");
-                   res.redirect('/signup');
-
-               }
-             
-        });
+        if(password === confirmPassword){
+            var user = {
+                firstName: firstName,
+                lastName: lastName,
+                password: password,
+                email: email,
+                username: username,
+                confirmPassword: confirmPassword,
+                priorityLevel: priorityLevel
+            }
     
+            
+    
+            var MongoClient = require('mongodb').MongoClient;
+            var url = "mongodb://localhost:27017/";
+    
+                MongoClient.connect(url, { useUnifiedTopology: true },function(err, db) {
+                    if (err) throw err;
+                     var dbo = db.db("arrows-express");
+                    dbo.collection("rider").insertOne(user, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+            
+                });
+            });
+    
+            res.redirect('/');
+        }
+        else{
+            console.log("passwords doesnt match");
+            res.redirect("/signup?err=nomatch");
+        }
         
     }
 }
