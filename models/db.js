fc53e696abbe53fb9750a3422804dc7bf58e9ab3
Aileen Sabellon <aileen.sabellon@gmail.com>
2020-04-25 20:26:28 +0800

    Changed Mongodb connection from local to atlas.

--- models/db.js
+++ models/db.js
@@ -17,7 +17,8 @@ const Admin = require('./AdminModel.js');
 const Reserve = require('./ReserveModel.js');
 
 // ccapdev-mongoose is the name of the database
-const url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/test?retryWrites=true&w=majority";
+//const url = 'mongodb://localhost:27017/arrows-express'; -> Local DB
+const url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/test?retryWrites=true&w=majority"; //Atlas DB
 
 // additional connection options
 const options = {
