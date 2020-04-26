84dcdd92d4bfa2bde744d27d5dfb5c9ae2b44515
Aileen Sabellon <aileen.sabellon@gmail.com>
2020-04-26 16:48:11 +0800

    forgot to save lol

--- controllers/editController.js
+++ controllers/editController.js
@@ -34,7 +34,7 @@ const editController = {
         */
         var MongoClient = require('mongodb').MongoClient;
         //var url = "mongodb://localhost:27017/";
-        var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/test?retryWrites=true&w=majority";
+        var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/arrows-express?retryWrites=true&w=majority";
         MongoClient.connect(url, { useUnifiedTopology: true },function(err, db) {
             if (err) throw err;
             var resultArray=[];
@@ -99,7 +99,7 @@ const editController = {
    
             var MongoClient = require('mongodb').MongoClient;
             //var url = "mongodb://localhost:27017/";
-            var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/test?retryWrites=true&w=majority";
+            var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/arrows-express?retryWrites=true&w=majority";
             MongoClient.connect(url, { useUnifiedTopology: true },function(err, db) {
                 if (err) throw err;
                 var resultArray=[];
@@ -132,7 +132,7 @@ const editController = {
 
             var MongoClient = require('mongodb').MongoClient;
             //var url = "mongodb://localhost:27017/";
-            var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/test?retryWrites=true&w=majority";
+            var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/arrows-express?retryWrites=true&w=majority";
             MongoClient.connect(url, { useUnifiedTopology: true },function(err, db) {
                 if (err) throw err;
                 var resultArray=[];
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -67,8 +67,8 @@ const profileController = {
 
         var MongoClient = require('mongodb').MongoClient;
         //var url = "mongodb://localhost:27017/";
-        var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/test?retryWrites=true&w=majority";
-        
+        var url = "mongodb+srv://arrows_express:password123!@cluster0-i9vbi.mongodb.net/arrows-express?retryWrites=true&w=majority";
+
         MongoClient.connect(url, { useUnifiedTopology: true },function(err, db) {
         if (err) throw err;
         var resultArray=[];
